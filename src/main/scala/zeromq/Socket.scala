8b43979f8e66c02b3ebc4b5bef807b1f4e352c3d
Tushar Mathur <tusharmath@gmail.com>
2020-04-27 20:30:43 +0530

    fix: tag info with Socket.Type

--- src/main/scala/zeromq/Socket.scala
+++ src/main/scala/zeromq/Socket.scala
@@ -8,26 +8,29 @@ import zio.Has
 import zio.RIO
 import zio.ZLayer
 import zio.ZIO
+import zio.Tagged
 
 object Socket {
-  final class Type(socketType: jeromq.SocketType) {
-    private type HasService = Has[Service]
-    trait Service {
-      def bind(address: String): Task[Boolean]
-      def close: Task[Unit]
-      def receive: Task[Option[Bytes]]
-      def receive(flags: Int): Task[Option[Bytes]]
-      def receiveString: Task[Option[String]]
-      def receiveString(flags: Int): Task[Option[String]]
-      def send(bytes: Bytes, flags: Int): Task[Boolean]
-      def send(bytes: Bytes): Task[Boolean]
-      def send(data: String, flags: Int): Task[Boolean]
-      def send(data: String): Task[Boolean]
-    }
+  private type HasService[A] = Has[Service[A]]
+  trait Service[A] {
+    def bind(address: String): Task[Boolean]
+    def close: Task[Unit]
+    def connect(address: String): Task[Boolean]
+    def receive: Task[Option[Bytes]]
+    def receive(flags: Int): Task[Option[Bytes]]
+    def receiveString: Task[Option[String]]
+    def receiveString(flags: Int): Task[Option[String]]
+    def send(bytes: Bytes, flags: Int): Task[Boolean]
+    def send(bytes: Bytes): Task[Boolean]
+    def send(data: String, flags: Int): Task[Boolean]
+    def send(data: String): Task[Boolean]
+  }
 
-    class Live private (jSocket: jeromq.ZMQ.Socket) extends Service {
+  final class Type[A <: jeromq.SocketType](socketType: A)(implicit t1: Tagged[Service[A]]) {
+    class Live private (jSocket: jeromq.ZMQ.Socket) extends Service[A] {
       override def bind(address: String): Task[Boolean]            = Task { jSocket.bind(address) }
       override def close: Task[Unit]                               = Task { jSocket.close() }
+      override def connect(address: String): Task[Boolean]         = Task { jSocket.connect(address) }
       override def receive: Task[Option[Bytes]]                    = Task { Option(jSocket.recv()) }
       override def receive(flags: Int): Task[Option[Bytes]]        = Task { Option(jSocket.recv(flags)) }
       override def receiveString: Task[Option[String]]             = Task { Option(jSocket.recvStr()) }
@@ -39,24 +42,22 @@ object Socket {
     }
 
     object Live {
-      def open(context: Context.Service): ZManaged[Any, Throwable, Service] =
+      def open(context: Context.Service): ZManaged[Any, Throwable, Service[A]] =
         context.createSocket(socketType).map { new Live(_) }.toManaged(_.close.orDie)
     }
 
-    def bind(address: String): RIO[HasService, Boolean]            = RIO.accessM(_.get.bind(address))
-    def close: RIO[HasService, Unit]                               = RIO.accessM(_.get.close)
-    def receive: RIO[HasService, Option[Bytes]]                    = RIO.accessM(_.get.receive)
-    def receive(flags: Int): RIO[HasService, Option[Bytes]]        = RIO.accessM(_.get.receive(flags))
-    def receiveString: RIO[HasService, Option[String]]             = RIO.accessM(_.get.receiveString)
-    def receiveString(flags: Int): RIO[HasService, Option[String]] = RIO.accessM(_.get.receiveString(flags))
-    def send(bytes: Bytes, flags: Int): RIO[HasService, Boolean]   = RIO.accessM(_.get.send(bytes, flags))
-    def send(bytes: Bytes): RIO[HasService, Boolean]               = RIO.accessM(_.get.send(bytes))
-    def send(string: String, flags: Int): RIO[HasService, Boolean] = RIO.accessM(_.get.send(string, flags))
-    def send(string: String): RIO[HasService, Boolean]             = RIO.accessM(_.get.send(string))
+    def bind(address: String): RIO[HasService[A], Boolean]            = RIO.accessM(_.get.bind(address))
+    def close: RIO[HasService[A], Unit]                               = RIO.accessM(_.get.close)
+    def connect(address: String): RIO[HasService[A], Boolean]         = RIO.accessM(_.get.connect(address))
+    def receive: RIO[HasService[A], Option[Bytes]]                    = RIO.accessM(_.get.receive)
+    def receive(flags: Int): RIO[HasService[A], Option[Bytes]]        = RIO.accessM(_.get.receive(flags))
+    def receiveString: RIO[HasService[A], Option[String]]             = RIO.accessM(_.get.receiveString)
+    def receiveString(flags: Int): RIO[HasService[A], Option[String]] = RIO.accessM(_.get.receiveString(flags))
+    def send(bytes: Bytes, flags: Int): RIO[HasService[A], Boolean]   = RIO.accessM(_.get.send(bytes, flags))
+    def send(bytes: Bytes): RIO[HasService[A], Boolean]               = RIO.accessM(_.get.send(bytes))
+    def send(string: String, flags: Int): RIO[HasService[A], Boolean] = RIO.accessM(_.get.send(string, flags))
+    def send(string: String): RIO[HasService[A], Boolean]             = RIO.accessM(_.get.send(string))
 
-    def live: ZLayer[Context, Throwable, HasService]                  = ZLayer.fromServiceManaged(Live.open(_))
-    def live(address: String): ZLayer[Context, Throwable, HasService] = ZLayer.fromServiceManaged(Live.open(_, address))
-    def use[R <: Has[_], E >: Throwable, A](zio: ZIO[HasService with R, E, A]): ZIO[Context with R, E, A] =
-      zio.provideSomeLayer[Context with R](live)
+    def live: ZLayer[Context, Throwable, HasService[A]] = ZLayer.fromServiceManaged(Live.open(_))
   }
 }
