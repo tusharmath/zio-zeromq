5808888dbc4ffa2bb8961bd613cfceb7f7d45389
Tushar Mathur <tusharmath@gmail.com>
2020-04-28 16:17:51 +0530

    test(socket): update tests

--- src/main/scala/zeromq/package.scala
+++ src/main/scala/zeromq/package.scala
@@ -2,9 +2,22 @@ import org.{zeromq => jeromq}
 import zio._
 
 package object zeromq {
-  type Bytes                          = Array[Byte]
+  type Bytes = Array[Byte]
+
   type Context                        = Has[Context.Service]
   type Socket[A <: Socket.SocketType] = Has[Socket.Service[A]]
+  type PairSocket                     = Socket[Socket.Pair.type]
+  type PubSocket                      = Socket[Socket.Pub.type]
+  type SubSocket                      = Socket[Socket.Sub.type]
+  type ReqSocket                      = Socket[Socket.Req.type]
+  type RepSocket                      = Socket[Socket.Rep.type]
+  type DealerSocket                   = Socket[Socket.Dealer.type]
+  type RouterSocket                   = Socket[Socket.Router.type]
+  type PullSocket                     = Socket[Socket.Pull.type]
+  type PushSocket                     = Socket[Socket.Push.type]
+  type XPubSocket                     = Socket[Socket.XPub.type]
+  type XSubSocket                     = Socket[Socket.XSub.type]
+  type StreamSocket                   = Socket[Socket.Stream.type]
 
   lazy val Pair   = new Socket.Type(Socket.Pair)
   lazy val Pub    = new Socket.Type(Socket.Pub)
--- src/test/scala/zeromq/SocketSpec.scala
+++ src/test/scala/zeromq/SocketSpec.scala
@@ -8,39 +8,35 @@ import zio._
 import zio.test.environment.TestConsole
 
 object SocketSpec extends DefaultRunnableSpec {
-  private def L = Context.live >>> (Req.live ++ Rep.live)
+  private def L: ZLayer[Any, Nothing, ReqSocket with RepSocket] = Context.live >>> (Req.live ++ Rep.live)
   def spec =
-    suite("Socket")(
-      suite("bind()") {
-        testM("should bind with an address") {
-          for {
-            _ <- Req.bind("ipc://tmp/ABC.sock")
-          } yield assertCompletes
-        }.provideCustomLayer(L)
-      } @@ ignore,
-      suite("REQ/REP")(
-        testM("should connect with each other") {
-          checkM(MGen.inproc, MGen.inproc) { (reqA, repA) =>
-            (for {
-              _      <- Req.bind(reqA)
-              _      <- Rep.bind(repA)
-              actual <- Req.connect(repA)
-            } yield assert(actual)(isTrue)).provideCustomLayer(L)
-          }
-        } @@ ignore,
-        testM("should send messages") {
-          checkM(MGen.inproc, Gen.alphaNumericStringBounded(2, 10)) { (addr, str) =>
-            val rep = Rep.bind(addr) *> Rep.receiveString >>= (k => console.putStrLn(k.toString()))
-            val req = Req.connect(addr) &&& Req.send(str)
-
-            for {
-              _        <- (rep <&> req).provideCustomLayer(L)
-              actual   <- TestConsole.output
-              expected = List(Option(str).toString())
-              _        <- TestConsole.clearOutput
-            } yield assert(actual.toList)(equalTo(expected))
-          }
+    suite("REQ/REP")(
+      testM("should bind with an address") {
+        for {
+          _ <- Req.bind("ipc://tmp/ABC.sock")
+        } yield assertCompletes
+      }.provideSomeLayer(L),
+      testM("should connect with each other") {
+        checkM(MGen.inproc, MGen.inproc) { (reqA, repA) =>
+          (for {
+            _      <- Req.bind(reqA)
+            _      <- Rep.bind(repA)
+            actual <- Req.connect(repA)
+          } yield assert(actual)(isTrue)).provideSomeLayer(L)
         }
-      )
-    )
+      },
+      testM("should send messages") {
+        checkM(MGen.inproc, Gen.anyString) { (addr, str) =>
+          val rep = Rep.bind(addr) *> Rep.receiveString >>= (k => console.putStr(k.toString()))
+          val req = Req.connect(addr) &&& Req.send(str)
+
+          (for {
+            _        <- rep <&> req
+            actual   <- TestConsole.output
+            expected = Vector(Option(str).toString())
+            _        <- TestConsole.clearOutput
+          } yield assert(actual)(equalTo(expected))).provideSomeLayer[ZTestEnv with ZEnv](L)
+        }
+      }
+    ) @@ silent @@ timeout(5 second)
 }
